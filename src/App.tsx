33f60d895c7f0e7a5dd85e3f968b0eb083f9b664
LoupAlphaa <153590529+LoupAlphaa@users.noreply.github.com>
2026-05-30 23:48:47 +0900

    Revert "Dev"

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
-import { Outlet } from 'react-router-dom'
-import Navbar from './components/layout/Navbar'
 import './App.css'
 
 function App() {
+
   return (
     <>
-      <Navbar />
-      <main className="main-content">
-        <Outlet />
-      </main>
+      <h1> Bienvenue dans le Temple </h1>
     </>
   )
 }
