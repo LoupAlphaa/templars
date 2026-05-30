2ee15d085676b502cf7f96fb9d400f6e21371fd5
Chirac Nathan <chiracnathan1@gmail.com>
2026-05-31 00:49:18 +0900

    Router added

---
+++ src/components/layout/Navbar.tsx
@@ -0,0 +1,21 @@
+import { Link } from 'react-router-dom'
+import './Navbar.css'
+
+function Navbar() {
+    return (
+        <nav className="navbar">
+            <div className="navbar-container">
+                <Link to="/" className="navbar-brand">
+                    <h1>⚔️ Templiers</h1>
+                </Link>
+                <ul className="navbar-menu">
+                    <li>
+                        <Link to="/">Accueil</Link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
